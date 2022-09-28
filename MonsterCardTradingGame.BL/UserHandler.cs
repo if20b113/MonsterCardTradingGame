7adc69bc54375adeabcd25784e409d468538e6b5
Florian Kretz <114428148+if20b113@users.noreply.github.com>
2022-09-28 12:38:02 +0200

    added user registration

---
+++ MonsterCardTradingGame.BL/UserHandler.cs
@@ -0,0 +1,19 @@
+﻿using MonsterCardTradingGame.DAL;
+using MonsterCardTradingGame.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterCardTradingGame.BL
+{
+    internal class UserHandler
+    {
+        public void registerUser(User newUser)
+        {
+            Server GameServer = new Server();
+            GameServer.registerUserDB(newUser);
+        }
+    }
+}
--- MonsterCardTradingGame.DAL/Server.cs
+++ MonsterCardTradingGame.DAL/Server.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using MonsterCardTradingGame.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,14 @@ namespace MonsterCardTradingGame.DAL
 {
     public class Server
     {
+        public void registerUserDB(User newUser)
+        {
+            //something will happen here soon
+
+
+        }
+
+
+
     }
 }
